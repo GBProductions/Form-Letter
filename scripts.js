bdd3443eb2ea724f8de1a6862540263ceb035a15
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-12 11:17:05 -0700

    Add script. place in HTML.
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 $(document).ready(function() {
+  $("#formOne").submit(function() {
+    const person1Input = $("input#person1").val();
+
+    $(".person1").text(person1Input);
+
+    $("#letter").show();
+    
+    event.preventDefault();
+  });
+});
